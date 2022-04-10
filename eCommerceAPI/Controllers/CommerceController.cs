00fcd0006283692fecc4287b790809da418373b9
Levi Araujo <leviaraujo182@gmail.com>
2022-04-10 17:37:26 -0300

    feature: Criação do método InsertProduct

--- eCommerceAPI/Controllers/CommerceController.cs
+++ eCommerceAPI/Controllers/CommerceController.cs
@@ -1,16 +1,36 @@
 ﻿using eCommerceAPI.Models;
+using eCommerceAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 
 namespace eCommerceAPI.Controllers
 {
     public class CommerceController : ControllerBase
     {
+        private CommerceRepository _repository;
+
+        public CommerceController()
+        {
+            _repository = new CommerceRepository();
+        }
+
         [Route("GetProduct")]
         [HttpGet]
-        public Product GetProduct()
+        public Product GetProduct(long id)
         {
-            return null;
+            Product product = new Product();
+
+            try
+            {
+                product = new CommerceRepository().GetProductById(id);
+            } 
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return product;
         }
 
         [Route("GetAllProducts")]
@@ -18,6 +38,20 @@ namespace eCommerceAPI.Controllers
         public List<Product> GetAllProducts()
         {
             return null;
+        }
+
+        [Route("InsertProduct")]
+        [HttpGet]
+        public void InsertProduct(Product product)
+        {
+            try 
+            {
+                _repository.InsertProduct(product);
+            } 
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
---
+++ eCommerceAPI/Repository/CommerceRepository.cs
@@ -0,0 +1,70 @@
+﻿using eCommerceAPI.Models;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace eCommerceAPI.Repository
+{
+    public class CommerceRepository
+    {
+        private DbConnection _connection;
+
+        public CommerceRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Commerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public Product GetProductById(long id)
+        {
+            Product product = new Product();
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = "SELECT * FROM Product WHERE Id = @Id";
+                sqlCommand.Parameters.AddWithValue("@Id", id);
+                sqlCommand.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    product.NameProduct = Convert.ToString(dataReader["NameProduct"]);
+                    product.Quantity = Convert.ToInt32(dataReader["Quantity"]);
+                    product.Value = Convert.ToDecimal(dataReader["Value"]);
+                }
+            }
+            catch(Exception ex)
+            {
+                _connection.Close();
+                throw new Exception(ex.Message);
+            }
+
+            return product;
+        }
+
+        public void InsertProduct(Product product)
+        {
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = "INSERT INTO Product VALUES (@NameProduct, @Quantity, @Value)";
+                sqlCommand.Parameters.AddWithValue("@NameProduct", product.NameProduct);
+                sqlCommand.Parameters.AddWithValue("@Quantity", product.Quantity);
+                sqlCommand.Parameters.AddWithValue("@Value", product.Value);
+                sqlCommand.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                _connection.Close();
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
