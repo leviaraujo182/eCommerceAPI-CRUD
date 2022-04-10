2927e2470d40e47d5c923f4af84199b01a3ade85
Levi Araujo <leviaraujo182@gmail.com>
2022-04-10 16:37:33 -0300

    feature: Criação dos métodos da ComerceController e criação da pasta models

---
+++ eCommerceAPI/Controllers/CommerceController.cs
@@ -0,0 +1,23 @@
+﻿using eCommerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace eCommerceAPI.Controllers
+{
+    public class CommerceController : ControllerBase
+    {
+        [Route("GetProduct")]
+        [HttpGet]
+        public Product GetProduct()
+        {
+            return null;
+        }
+
+        [Route("GetAllProducts")]
+        [HttpGet]
+        public List<Product> GetAllProducts()
+        {
+            return null;
+        }
+    }
+}
